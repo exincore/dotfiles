ea6db9fc5c278df8d95640b7c6ed42ab1e865a49
exin <me@exin.xyz>
2022-09-26 12:25:27 -0600

    Update starship path

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    ~/Documents/starship/target/release/starship init fish | source
-    set -gx PATH $PATH ~/.cargo/bin
+    ~/Documents/Repositories/starship/target/release/starship init fish | source
 end
