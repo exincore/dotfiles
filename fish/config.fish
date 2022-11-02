a0025219b9b601dce4f33df5ea990c6efd020982
exin <me@exin.xyz>
2022-11-02 15:43:34 -0600

    Add starship variables and templating

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,5 @@
 if status is-interactive
-    set -l STARSHIP ~/Documents/Repositories/starship/target
-    if test -e $STARSHIP/release/starship
-        $STARSHIP/release/starship init fish | source
-    else if command -sq starship
-        starship init fish | source
-    end
+    {{#if dotter.packages.starship}}
+    {{starship.executable}} init fish | source
+    {{/if}}
 end
